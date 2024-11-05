2f6b6f80cfdd3d203c16aab5f6043a2d1c22e2fe
hari-ft <hariprasadft@gmail.com>
2024-11-05 15:55:28 +0530

    Update waagent_install.sh

--- waagent_install.sh
+++ waagent_install.sh
@@ -1,10 +1,28 @@
-sudo pkg update
-sudo pkg install git
+#!/bin/sh
+
+# Install required packages
+pkg install -y git
+
+# Create symlink for Python if not already present
 ln -sf /usr/local/bin/python3 /usr/local/bin/python
+
+# Clone the WALinuxAgent repository
 git clone https://github.com/Azure/WALinuxAgent.git
+
+# Navigate to the WALinuxAgent directory
 cd WALinuxAgent
+
+# Install WALinuxAgent and register it as a service
 python3 setup.py install --register-service
+
+# Enable the waagent service
 sysrc waagent_enable="YES"
+
+# Start the waagent service
 service waagent start
+
+# Wait for 10 seconds before checking the status
 sleep 10
+
+# Check the status of waagent service
 service waagent status
